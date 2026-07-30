677961e7d72936f26ce8c6cdfa042bc990b2e785
Artem Yevchenko <yevchenko4rtem@gmail.com>
2026-07-30 22:18:48 +0200

    nout-dead

--- brains.js
+++ brains.js
@@ -14,7 +14,11 @@ const body = document.querySelector("body"),
     cancel = document.querySelector(".cancel"),
     apply = document.querySelector(".apply"),
 
-    navC = document.querySelector(".nav-list");
+    navC = document.querySelector(".nav-list"),
+
+    styleing = document.querySelector(".styleing"),
+
+    locked = document.querySelector(".locked");
 
 navOpenClose.forEach(function (button) {
     button.addEventListener("click", () => {
@@ -27,17 +31,7 @@ navC.addEventListener("click", (event) => {
     parent.classList.toggle("open1");
 })
 
-
-postNav.addEventListener("click", (event) => {
-    if (event.target.classList.contains("lock2")) {
-        event.target.parentElement.classList.toggle("lock-vertical")
-    }
-    if(event.target.classList.contains("mid1")){
-        postsCon.classList.toggle("visible")
-    }
-})
-
-const n = ()=> {
+const n = () => {
     const clear = '';
     const text = getDescr.value
     console.log(text);
@@ -46,3 +40,23 @@ const n = ()=> {
 }
 apply.addEventListener("click", n)
 
+styleing.addEventListener("click", (event) => {
+    if (event.target.classList.contains("fa-moon")) {
+        event.target.classList.replace("fa-moon", "fa-sun");
+        body.classList.replace("style-black", "style-white");
+    } else if (event.target.classList.contains("fa-sun")) {
+        event.target.classList.replace("fa-sun", "fa-moon");
+        body.classList.replace("style-white", "style-black");
+    }
+})
+
+locked.addEventListener("click", (event) => {
+    if (event.target.classList.contains("fa-lock-open")) {
+        event.target.classList.replace("fa-lock-open", "fa-lock");
+        navVisible.classList.add("lock3");
+    }
+    else if (event.target.classList.contains("fa-lock")) {
+        event.target.classList.replace("fa-lock", "fa-lock-open");
+        navVisible.classList.remove("lock3");
+    }
+})
