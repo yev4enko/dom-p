d45d8a6f2dfe1cf92baeb9946aace5f8cc73e90e
Artem Yevchenko <yevchenko4rtem@gmail.com>
2026-08-09 16:40:56 +0200

    c=8

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,5 +1,34 @@
 import { printModal } from "./modal.js";
 
+const post = function (arr) {
+    if (arr === null || arr === undefined) {
+        return '';
+    }
+    let postContent = `<div id="postID${arr.postID}"class="post">
+        <div class="post-upper">
+            <div class = "post-upper-text">
+                <a href="#postID-${arr.postID}" id="content-extend" class="tittle">${arr.title}</a>
+                <div class="author">by.${arr.authorName}</div>
+                <div class="saved">${arr.savedPost}</div>
+                <div class="tags">${arr.tags}</div>
+            </div>
+            <div class="post-descr">   
+                <div class="descr">${arr.description}</div>
+            </div>
+        </div> 
+        <div class="post-nav">
+            <button id = "save-nav" class="post-btn"><i class="fa-solid fa-bookmark"></i></button>
+            <button id = "edit-nav" class="post-btn"><i class="fa-solid fa-pen"></i></button>
+            <button id = "remove-nav" class="post-btn"><i class="fa-solid fa-trash"></i></button>
+        </div> 
+    </div>`
+    return postContent;
+}
+
+function removePost(arr) {
+    arr.remove()
+}
+
 export function getData() {
     return fetch('./posts.json')
         .then((responce) => { return responce.json() })
@@ -7,31 +36,78 @@ export function getData() {
 
 export function printPost(arr) {
     const contentContainer = document.querySelector(".content");
-    contentContainer.innerHTML +=
-        `<div class="post">
-            <button class="content-btn"></button>
-            <button class="modal-btn"></button>
-            <h1 class="author">${arr.authorName}</h1>
-            <h1 class="tittle">${arr.title}</h1>
-            <div class="descr">${arr.description}</descr>
-        </div>`
+    contentContainer.innerHTML += post(arr)
 }
 
 export function eventHandler() {
-    const container = document.querySelector(".content");
+    const contentContainer = document.querySelector(".content");
+    const searchContainer = document.querySelector(".search-container")
 
-    container.addEventListener("click", (event) => {
+    contentContainer.addEventListener("click", e => {
+        const target = e.target
+        const link = target.closest("#content-extend")
+        if (!link) {
+            return;
+        }
+        const post = link.closest(".post")
+        const text = post.querySelector(".descr");
+        text.classList.toggle("extended")
+        post.classList.toggle("extended")
+    })
+    contentContainer.addEventListener("click", e => {
+        const target = e.target
+        const saveButton = target.closest("#save-nav");
+        if (!saveButton) {
+            return;
+        }
+        const post = saveButton.closest(".post")
+        const saveCounter = post.querySelector(".saved")
+        const postOjb = JSON.parse(localStorage.getItem(`${post.id}`))
 
-        const button = event.target.closest(".content-btn");
-        const modalButton = event.target.closest(".modal-btn");
-        if (event.target.closest(".content-btn")) {
-            if (!button) return;
-            const post = button.closest(".post");
-            post.classList.toggle("extended")
+        postOjb.savedPost = postOjb.savedPost === true ? false : true;
+        const postObjString = JSON.stringify(postOjb)
+        localStorage.setItem(`${post.id}`, postObjString)
+        const postPostObj = JSON.parse(localStorage.getItem(`${post.id}`))
+        saveCounter.textContent = postPostObj.savedPost
+
+    })
+    contentContainer.addEventListener("click", e => {
+        const target = e.target
+        const deleteButton = target.closest("#remove-nav");
+        if (!deleteButton) {
+            return;
         }
-        if (event.target.closest(".modal-btn")) {
-            if (!modalButton) return;
-            printModal();
+        const post = deleteButton.closest(".post")
+        localStorage.removeItem(`${post.id}`)
+        removePost(post)
+    })
+
+}
+
+export function eventHandlerSearch() {
+    const searchContainer = document.querySelector(".search-container")
+    searchContainer.addEventListener("click", (e) => {
+        const target = e.target;
+        const savedFilter = target.closest("#saved-posts")
+        const posts = document.querySelectorAll(".post")
+        if (!savedFilter) {
+            return
         }
-    });
+        let postsObj = getAllPostsFromLSByKey("postID")
+        postsObj.filter(p => p.savedPost === true)
+    })
+
+}
+
+
+function getAllPostsFromLSByKey(key) {
+    const posts = []
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key.startsWith(key)) {
+            const post = JSON.parse(localStorage.getItem(key));
+            posts.push(post);
+        }
+    }
+    return posts;
 }
